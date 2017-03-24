65199c9679a8b8425bb83e8b4916346ac7c13d0d
weng wang <wengwangjay@126.com>
2017-03-24 18:04:37 +0800

    cssnano 测试

--- gulp-es2015/gulp/html.js
+++ gulp-es2015/gulp/html.js
@@ -15,15 +15,11 @@ const stream = require('./server').stream;
 
 function html(){
 	return gulp.src(path.join(_path.src,_folder.html))
-			.pipe(cached('html'))
 			.pipe(fileInclude({
 				prefix: '@@',
 				basePath: path.join(_path.src,_folder.html)
 			}))
-			.pipe(htmlReplace({
-				'css': 'styles/common.css',
-				'js': 'scripts/bundle.js'
-			}))
+			.pipe(cached('html'))
 			.pipe(gulp.dest(_path.dist))
 			.pipe(stream())
 }
--- gulp-es2015/gulp/styles.js
+++ gulp-es2015/gulp/styles.js
@@ -6,21 +6,29 @@ const gulp = require('gulp');
 const cached = require('gulp-cached');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const nano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 
 const _path = require('./config').path;
 const _folder = require('./config').folder;
 
 const stream = require('./server').stream;
 
-function styles(){
-	return gulp.src(path.join(_path.src, _folder.css))
-		.pipe(cached('styles'))
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest(_path.dist))
-		.pipe(stream())
+function styles() {
+    return gulp.src(path.join(_path.src, _folder.css))
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(nano({ 
+        	zindex: false, 
+        	reduceIdents: false 
+        }))
+        .pipe(cached('styles'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(_path.dist))
+        .pipe(stream())
 };
 
 module.exports = styles;
